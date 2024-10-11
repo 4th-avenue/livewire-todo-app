fe2f03e283bd006d9f3ece60b6c8b16d823b0329
4th-avenue <quebec706@gmail.com>
2024-10-11 17:19:02 +0900

    Todo 모델에 fillable 정의  후 새 Todo 등록 기능 구현

--- app/Livewire/TodoList.php
+++ app/Livewire/TodoList.php
@@ -2,10 +2,28 @@
 
 namespace App\Livewire;
 
+use App\Models\Todo;
 use Livewire\Component;
+use Livewire\Attributes\Rule;
 
 class TodoList extends Component
 {
+    #[Rule('required|min:3|max:50')]
+    public $name;
+
+    public $search;
+
+    public function create()
+    {
+        $validated = $this->validateOnly('name');
+
+        Todo::create($validated);
+
+        $this->reset('name');
+
+        session()->flash('success', 'Created.');
+    }
+
     public function render()
     {
         return view('livewire.todo-list');
